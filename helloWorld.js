9df3c63580f216aba58d13aefd4510c2de8b2831
Jacob Erickson <Jakerson8@gmail.com>
2022-06-21 14:39:42 -0600

    wrote new lines, age and channel

---
+++ helloWorld.js
@@ -0,0 +1,32 @@
+console.log("hello, universe");
+
+const firstName = "Jacob";
+
+let age = 22;
+
+let isCool = true;
+
+let x = 4;
+let y = 10;
+
+let z = 7+ 9;
+let sum = x+y+z;
+let diff = (y+x)-(y+z)
+
+let string = 'Hello, Universe'
+
+let welcomeString = string + " "+ firstName
+console.log(welcomeString);
+if(age > 21){
+    console.log(`you can drink ${firstName}`)
+} else{
+    console.log(`you are underage`)
+}
+
+let score = 45
+let passing = 40
+if(score > passing){
+    console.log(`you passed`)
+} else{
+    console.log(`you failed`)
+}
